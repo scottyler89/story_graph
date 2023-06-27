9930d5181f3e019a0a67f458593c193393ac1677
scottyler892 <scottyler89+bitbucket@gmail.com>
2023-06-26 22:04:01 -0400

    Add initial unit tests for narrative graph classes

--- tests/test_narrative_graph.py
+++ tests/test_narrative_graph.py
@@ -1,8 +1,24 @@
 
 import unittest
+from src.narrative_graph import Node, Edge, Graph, Narrative
 
 class TestNarrativeGraph(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.node = Node('Test')
+        self.graph = Graph()
+        self.narrative = Narrative()
+
+    def test_node_creation(self):
+        self.assertEqual(self.node.name, 'Test')
+        self.assertEqual(self.node.traits, {})
+        self.assertEqual(self.node.connections, [])
+
+    def test_graph_creation(self):
+        self.assertEqual(self.graph.nodes, {})
+        self.assertEqual(self.graph.edges, [])
+
+    def test_narrative_creation(self):
+        self.assertEqual(self.narrative.graphs, {})
 
 if __name__ == '__main__':
     unittest.main()
